d888ffc789af6fa9a51f6d7c9970204670d96682
TazawaAyumu <tazawaayumu@gmail.com>
2022-02-03 20:19:04 +0900

    確認メールアドレス用のTextFieldを追加

--- BookReviewApp/Login/LoginView.swift
+++ BookReviewApp/Login/LoginView.swift
@@ -26,6 +26,13 @@ class Login: UIView {
         return mailTextField
     }()
     
+    let confirmationEmailTextField: UITextField = {
+        let mailTextField = UITextField()
+        mailTextField.placeholder = "確認メールアドレスを入力してください"
+        mailTextField.borderStyle = .roundedRect
+        return mailTextField
+    }()
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
         
@@ -42,6 +49,7 @@ class Login: UIView {
     func setup() {
         self.addSubview(logInButton)
         self.addSubview(emailTextField)
+        self.addSubview(confirmationEmailTextField)
 
         logInButton.snp.makeConstraints { make in
             make.centerX.equalToSuperview()
@@ -56,44 +64,13 @@ class Login: UIView {
             make.centerX.equalToSuperview()
             make.bottom.equalTo(logInButton.snp.top).offset(-50)
         }
+        
+        confirmationEmailTextField.snp.makeConstraints { make in
+            make.width.equalTo(200)
+            make.height.equalTo(50)
+            make.centerX.equalToSuperview()
+            make.bottom.equalTo(emailTextField.snp.top).offset(-50)
+        }
     }
-    //メール入力テキストフィールド
-//    func setEmailTextField() {
-//
-//
-//        let emailTextFieldSize: CGRect = CGRect(x: 20, y: 400,width: 300,height: 60)
-//        let mailTextField = UITextField(frame: emailTextFieldSize)
-//        mailTextField.placeholder = "メールアドレスを入力してください"
-//        mailTextField.borderStyle = .roundedRect
-//
-//        self.addSubview(mailTextField)
-//        mailTextField.snp.makeConstraints { make in
-//            make.width.equalTo(200)
-//            make.height.equalTo(50)
-//            make.bottom.equalTo(button.snp.top).offset(-10)
-//            make.centerX.equalToSuperview()
-//        }
-//    }
-    
-    //ログインボタン
-//    func setUpButton() {
-//        let buttonSize: CGRect = CGRect(x: 100, y: 600,width: 200,height: 60)
-//        self.button = UIButton(frame: buttonSize)
-//        button.setTitle("ログイン", for: .normal)
-//        button.backgroundColor = UIColor.cyan
-//        button.setTitleColor(.systemBlue, for: .normal)
-//
-//        self.addSubview(button)
-//        button.snp.makeConstraints { make in
-//            let width: CGFloat = self.frame.width
-//            print(width)
-//            make.width.equalTo(200)
-//            make.height.equalTo(50)
-//            make.center.equalToSuperview()
-//        }
-//    }
-//
-
-    
 
 }
