419934685ab5d082187a409192c53d8d7c01c43e
TazawaAyumu <tazawaayumu@gmail.com>
2022-02-20 20:32:14 +0900

    画面遷移処理を追加

--- BookReviewApp/Top/TopViewController.swift
+++ BookReviewApp/Top/TopViewController.swift
@@ -12,13 +12,24 @@ class TopViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         view.backgroundColor = .white
+        
+        navigationItem.title = "Top画面"
+        
         self.topView = TopView()
         self.topView.frame = CGRect(x: 0, y: 0, width: self.view.bounds.width, height: self.view.bounds.height)
         self.view.addSubview(topView)
 
-        
+        topView.toLogInButton.addTarget(self, action: #selector(didTapButton), for: .touchDown)
     }
     
+    @objc func didTapButton(_ sender: UIButton) {
+
+        let secondPageViewController = LoginViewController()
+         navigationController?.pushViewController(secondPageViewController, animated: true)
+
+    }
+        
 
 }
