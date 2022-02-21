dfb62b301d4ddb720a2c3c1baeb9cef795aeec78
TazawaAyumu <tazawaayumu@gmail.com>
2022-02-21 23:56:53 +0900

    initial commit

---
+++ BookReviewApp/Login/LoginValue.swift.swift
@@ -0,0 +1,13 @@
+//
+//  LoginValue.swift.swift
+//  BookReviewApp
+//
+//  Created by 田澤歩 on 2022/02/21.
+//
+
+import Foundation
+
+struct LoginValue {
+    var email: String?
+    var password: String?
+}
