5a0369bd9740003c371400d73f71b75f8e2ea7e0
TazawaAyumu <tazawaayumu@gmail.com>
2022-02-26 22:35:14 +0900

    コードを関数に整理

--- BookReviewApp/Login/LoginViewController.swift
+++ BookReviewApp/Login/LoginViewController.swift
@@ -9,20 +9,35 @@ import UIKit
 
 class LoginViewController: UIViewController {
     var loginView:Login! = Login()
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .white
-        self.loginView = Login()
-        loginView.frame = CGRect(x: 0, y: 0, width: self.view.bounds.width, height: self.view.bounds.height)
-
-        self.view.addSubview(loginView)
-        loginView.logInButton.addTarget(self, action: #selector(didTapButton), for: .touchDown)
         
+        view.backgroundColor = .white
+        
+        setUpView()
+        setUpButton()
+        
+    }
+    
+    func setUpView() {
+        let width = self.view.bounds.width
+        let height = self.view.bounds.height
+        self.loginView = Login()
+        loginView.frame = CGRect(x: 0, y: 0, width: width, height: height)
+        self.view.addSubview(loginView)
+    }
+    
+    func setUpButton() {
+        loginView.logInButton.addTarget(self,
+                                        action: #selector(didTapButton),
+                                        for: .touchDown)
     }
     
     @objc func didTapButton() {
         print("ボタンがタップされました")
+        let secondPageViewController = ViewController.init()
+        navigationController?.pushViewController(secondPageViewController, animated: true)
     }
 
 
