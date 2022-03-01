f92a834846b928e9ade188a3b7c9c65fc200b8b7
TazawaAyumu <tazawaayumu@gmail.com>
2022-03-01 22:48:20 +0900

    viewDidLoadを整理

--- BookReviewApp/Top/TopViewController.swift
+++ BookReviewApp/Top/TopViewController.swift
@@ -14,22 +14,37 @@ class TopViewController: UIViewController {
         super.viewDidLoad()
         
         view.backgroundColor = .white
-        
         navigationItem.title = "Top画面"
-        
+        setUpView()
+        setUpButton()
+
+    }
+    
+    
+    func setUpView() {
         self.topView = TopView()
         self.topView.frame = CGRect(x: 0, y: 0, width: self.view.bounds.width, height: self.view.bounds.height)
         self.view.addSubview(topView)
-
-        topView.toLogInButton.addTarget(self, action: #selector(didTapButton), for: .touchDown)
     }
     
-    @objc func didTapButton(_ sender: UIButton) {
-
-        let secondPageViewController = LoginViewController()
-         navigationController?.pushViewController(secondPageViewController, animated: true)
-
+    func setUpButton() {
+        topView.toLogInButton.addTarget(self,
+                                        action: #selector(didTapButton),
+                                        for: .touchDown)
+        topView.toSignInButton.addTarget(self,
+                                         action: #selector(didTaptoSignInButton),
+                                         for: .touchDown)
     }
-        
+    
+    
+    @objc func didTapButton(_ sender: UIButton) {
+        let vc = LoginViewController()
+         navigationController?.pushViewController(vc, animated: true)
+    }
+    
+    @objc func didTaptoSignInButton(_ sender: UIButton) {
+        let signInViewController = SignInViewController()
+        navigationController?.pushViewController(signInViewController, animated: true)
+    }
 
 }
