a64e4d8c2b617e1e35c0e9d3a52711fdd32a5da0
TazawaAyumu <tazawaayumu@gmail.com>
2022-03-08 22:53:27 +0900

    Initial commit

--- BookReviewApp/Common/Dialog.swift
+++ BookReviewApp/Common/Dialog.swift
@@ -8,10 +8,11 @@ import UIKit
 
 class Dialog {
     
-    func showDialog(vc: ViewController) {
-        let dialog = UIAlertController(title: "エラー", message: "操作をやり直してください", preferredStyle: .alert)
+    func showDialog(vc: UIViewController, massegae: String) {
+        let dialog = UIAlertController(title: "エラー", message: "\(massegae)\n操作をやり直してください", preferredStyle: .alert)
         dialog.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
         vc.present(dialog, animated: true, completion: nil)
+        
     }
 
 }
