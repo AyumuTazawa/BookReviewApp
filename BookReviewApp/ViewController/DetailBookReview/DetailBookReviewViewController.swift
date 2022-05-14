04a4c6308af81a89c234014eb04c1968c180892d
TazawaAyumu <tazawaayumu@gmail.com>
2022-05-14 23:32:14 +0900

    クラス名修正

--- BookReviewApp/ViewController/DetailBookReview/DetailBookReviewViewController.swift
+++ BookReviewApp/ViewController/DetailBookReview/DetailBookReviewViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class BookReviewDetailViewController: UIViewController {
+class DetailBookReviewViewController: UIViewController {
 
     var bookReviewDetailView = BookReviewDetailView()
     
