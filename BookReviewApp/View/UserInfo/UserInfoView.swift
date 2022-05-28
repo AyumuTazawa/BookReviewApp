26b58eaecae82a96f9dffca9767a592a33bbece0
TazawaAyumu <tazawaayumu@gmail.com>
2022-05-28 22:32:41 +0900

    UIKitに修正

--- BookReviewApp/View/UserInfo/UserInfoView.swift
+++ BookReviewApp/View/UserInfo/UserInfoView.swift
@@ -5,51 +5,56 @@
 //  Created by 田澤歩 on 2022/02/28.
 //
 
-import SwiftUI
+import UIKit
+import SnapKit
 
-struct UserInfoView: View {
+class UserInfoView: UIView {
     
-    @State var name: String = ""
+    public var editUserInfoButton: UIButton! = { () -> UIButton in
+        let button = UIButton()
+        button.setTitle("ユーザー情報更新", for: .normal)
+        button.backgroundColor = UIColor.cyan
+        button.setTitleColor(.systemBlue, for: .normal)
+        
+        return button
+    }()
     
-    var body: some View {
+    let nameTextField: UITextField = { () -> UITextField in
+        let mailTextField = UITextField()
+        mailTextField.placeholder = "ユーザー名"
+        mailTextField.borderStyle = .roundedRect
+        return mailTextField
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
         
-        var errMessage: [String] = []
-        let userInfoViewBounds = UIScreen.main.bounds
-        let userInfoVieWwidth = userInfoViewBounds.width
-        let userInfoViewHeight = Int(userInfoViewBounds.height)
-        let uiWidth = userInfoVieWwidth / 1.3
+        setup()
+    }
+    
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
         
-        ZStack {
-            Color.yellow
-                .ignoresSafeArea(.all)
-            
-            VStack {
-                //タイトル
-                Text("ユーザー情報更新")
-                    .foregroundColor(.green)
-                
-                //名前
-                TextField("名前", text: $name)
-                    .textFieldStyle(.roundedBorder)
-                    .frame(width: uiWidth)
-                    .padding()
-                
-                //名前更新ボタン
-                Button("名前を更新", action: {
-                    print("名前を更新ボタン押下")
-                    let checkNameResult = Validator.shared.checkName(name: name, min: 1, max: 50)
-                    if(checkNameResult.isValid == false){ errMessage.append(checkNameResult.isError)
-                    }
-                    if (errMessage.isEmpty){
-                        print("OK")
-                    }else{
-                        print(errMessage)
-                    }
-                })
-                    .frame(width: uiWidth, height: 40)
-                    .background(.pink)
-                
-            }
+        setup()
+    }
+    
+    func setup() {
+        self.addSubview(editUserInfoButton)
+        self.addSubview(nameTextField)
+
+        editUserInfoButton.snp.makeConstraints { make in
+            make.centerX.equalToSuperview()
+            make.centerY.equalToSuperview()
+            make.width.equalTo(200)
+            make.height.equalTo(50)
+        }
+        
+        nameTextField.snp.makeConstraints { make in
+            make.width.equalTo(200)
+            make.height.equalTo(50)
+            make.centerX.equalToSuperview()
+            make.bottom.equalTo(editUserInfoButton.snp.top).offset(-50)
         }
     }
 }
