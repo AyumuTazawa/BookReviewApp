fe7e51f6462573943442814213b072fbd1421564
TazawaAyumu <tazawaayumu@gmail.com>
2022-06-01 12:42:59 +0900

    ユーザートークン取得処理修正

--- BookReviewApp/Model/PostBookReview/PostBookReviewModel.swift
+++ BookReviewApp/Model/PostBookReview/PostBookReviewModel.swift
@@ -14,7 +14,8 @@ protocol PostBookReviewProtocol {
 
 class PostBookReviewModel: PostBookReviewProtocol {
     func postBookReview(postBookData: Dictionary<String,String>) {
-        let headers = self.createHeaders()
+        let userToken: UserToken = UserToken()
+        let headers = userToken.getHeaders()
         let url = "https://api-for-missions-and-railways.herokuapp.com/books"
         
         AF.request(url, method: .post, parameters: postBookData, encoding: JSONEncoding.default, headers: headers).responseData { response in
@@ -32,51 +33,5 @@ class PostBookReviewModel: PostBookReviewProtocol {
             }
         }
     }
-    
-    func createHeaders() -> HTTPHeaders {
-        var token = self.loadToken()!
-        token = "Bearer \(token)"
-        let headers: HTTPHeaders = [
-            "Authorization": token
-        ]
-        
-        return headers
-    }
-    
-    
-    func loadToken() -> String? {
-        let id = "id"
-        let key = "userToken"
-        
-        let query: [String: Any] = [
-            kSecClass              as String: kSecClassGenericPassword,
-            kSecAttrService        as String: key,
-            kSecAttrAccount        as String: id,
-            kSecMatchLimit         as String: kSecMatchLimitOne,
-            kSecReturnAttributes   as String: true,
-            kSecReturnData         as String: true,
-        ]
-        
-        var item: CFTypeRef?
-        let status = SecItemCopyMatching(query as CFDictionary, &item)
-        switch status {
-        case errSecItemNotFound:
-            return nil
-        case errSecSuccess:
-            guard let item = item,
-                  let value = item[kSecValueData as String] as? Data else {
-                      print("トークンデータなし")
-                      return nil
-                  }
-            guard let loadString = String(data: value, encoding: .utf8) else {
-                return nil
-            }
-            return loadString
-        default:
-            print("該当なし")
-        }
-        return nil
-    }
-    
     
 }
