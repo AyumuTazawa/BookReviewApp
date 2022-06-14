284c8cee1e00ba264ddd5e2c2ed6fcbb6867afb0
TazawaAyumu <tazawaayumu@gmail.com>
2022-06-15 06:12:11 +0900

    initial commit

---
+++ BookReviewApp/Model/User/SignIn.swift
@@ -0,0 +1,30 @@
+//
+//  SignIn.swift
+//  BookReviewApp
+//
+//  Created by 田澤歩 on 2022/06/14.
+//
+
+import Foundation
+
+class SignIn {
+    
+    let name: UserName
+    let email: Email
+    let password: Password
+    
+    init(name: UserName, email: Email, password: Password) {
+        self.name = name
+        self.email = email
+        self.password = password
+    }
+    
+    func makePostData() -> Dictionary<String, String> {
+        var signindata: Dictionary<String, String> = [:]
+        signindata["name"] = self.name.toString()
+        signindata["email"] = self.email.toString()
+        signindata["password"] = self.password.toString()
+        
+        return signindata
+    }
+}
