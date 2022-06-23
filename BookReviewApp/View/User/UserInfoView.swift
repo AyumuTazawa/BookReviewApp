df8c9d994ef82a8778340f8e56b3343519b83fc4
TazawaAyumu <tazawaayumu@gmail.com>
2022-06-23 20:20:56 +0900

    レイアウト修正

--- BookReviewApp/View/User/UserInfoView.swift
+++ BookReviewApp/View/User/UserInfoView.swift
@@ -10,7 +10,28 @@ import SnapKit
 
 class UserInfoView: UIView {
     
-    public var editUserInfoButton: UIButton! = { () -> UIButton in
+    public let topBackView: UIView = { () -> UIView in
+        let view: UIView = UIView()
+        view.backgroundColor = .systemPink
+        
+        return view
+    }()
+    
+    public let centerBackView: UIView = { () -> UIView in
+        let view: UIView = UIView()
+        view.backgroundColor = .systemMint
+        
+        return view
+    }()
+    
+    public let bottomBackView: UIView = { () -> UIView in
+        let view: UIView = UIView()
+        view.backgroundColor = .systemBlue
+        
+        return view
+    }()
+    
+    public let editUserInfoButton: UIButton! = { () -> UIButton in
         let button = UIButton()
         button.setTitle("ユーザー情報更新", for: .normal)
         button.backgroundColor = UIColor.cyan
@@ -19,10 +40,12 @@ class UserInfoView: UIView {
         return button
     }()
     
-    let nameTextField: UITextField = { () -> UITextField in
+    public let nameTextField: UITextField = { () -> UITextField in
         let mailTextField = UITextField()
         mailTextField.placeholder = "ユーザー名"
         mailTextField.borderStyle = .roundedRect
+        mailTextField.becomeFirstResponder()
+        
         return mailTextField
     }()
     
@@ -40,21 +63,46 @@ class UserInfoView: UIView {
     }
     
     func setup() {
-        self.addSubview(editUserInfoButton)
-        self.addSubview(nameTextField)
-
-        editUserInfoButton.snp.makeConstraints { make in
+        self.addSubview(centerBackView)
+        self.addSubview(topBackView)
+        self.addSubview(bottomBackView)
+        
+        topBackView.addSubview(nameTextField)
+        bottomBackView.addSubview(editUserInfoButton)
+        
+        centerBackView.snp.makeConstraints { make in
             make.centerX.equalToSuperview()
             make.centerY.equalToSuperview()
-            make.width.equalTo(200)
+            make.width.equalToSuperview()
             make.height.equalTo(50)
         }
         
-        nameTextField.snp.makeConstraints { make in
-            make.width.equalTo(200)
-            make.height.equalTo(50)
+        topBackView.snp.makeConstraints { make in
+            make.width.equalToSuperview()
+            make.height.equalToSuperview().multipliedBy(0.35)
             make.centerX.equalToSuperview()
-            make.bottom.equalTo(editUserInfoButton.snp.top).offset(-50)
+            make.bottom.equalTo(centerBackView.snp.top)
+        }
+        
+        bottomBackView.snp.makeConstraints { make in
+            make.width.equalToSuperview()
+            make.height.equalToSuperview().multipliedBy(0.35)
+            make.centerX.equalToSuperview()
+            make.top.equalTo(centerBackView.snp.bottom)
+        }
+        
+        nameTextField.snp.makeConstraints { make in
+            make.centerX.equalToSuperview()
+            make.bottom.equalTo(topBackView.snp.bottom).offset(-10)
+            make.width.equalToSuperview().multipliedBy(0.9)
+            make.height.equalTo(50)
+        }
+        
+        editUserInfoButton.snp.makeConstraints { make in
+            make.centerX.equalToSuperview()
+            make.top.equalTo(bottomBackView.snp.top).offset(0)
+            make.width.equalToSuperview().multipliedBy(0.9)
+            make.height.equalTo(50)
         }
     }
 }
