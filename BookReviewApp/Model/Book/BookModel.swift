165b49311fd59b4d9dd2d7e1641d2fd895a8b21e
TazawaAyumu <tazawaayumu@gmail.com>
2022-07-16 21:58:08 +0900

    メソッド追加

--- BookReviewApp/Model/Book/BookModel.swift
+++ BookReviewApp/Model/Book/BookModel.swift
@@ -8,30 +8,48 @@
 import Foundation
 
 class BookModel {
-    
+
     let id: String
-    let titile: String
+    let title: String
     let url: String
     let detail: String
     let review: String
-    let reviewer: String
+    //let reviewer: String
     let isMine: Bool
-    
+
     init(id: String,
          title: String,
          url: String,
          detail: String,
          review: String,
-         reviewer: String,
+         //reviewer: String,
          isMine: Bool
     ) {
         self.id = id
-        self.titile = title
+        self.title = title
         self.url = url
         self.detail = detail
-        self.reviewer = reviewer
+        //self.reviewer = reviewer
         self.review = review
         self.isMine = isMine
     }
+
+    func toDetail() -> String {
+        return self.detail
+    }
     
+    func toReview() -> String {
+        return self.review
+    }
+
+    func toArray() -> Dictionary<String, String> {
+        let array: Dictionary<String, String> = [
+            "title": self.title,
+            "url": self.url,
+            "detail": self.detail,
+            "review": self.review
+        ]
+        return array
+    }
+
 }
