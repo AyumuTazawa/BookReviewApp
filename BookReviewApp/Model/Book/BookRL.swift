6da309f65a6af84e8c90891f5f0652ce1cedc61a
TazawaAyumu <tazawaayumu@gmail.com>
2022-07-31 16:38:00 +0900

    initial commit

---
+++ BookReviewApp/Model/Book/BookRL.swift
@@ -0,0 +1,22 @@
+//
+//  BookRL.swift
+//  BookReviewApp
+//
+//  Created by 田澤歩 on 2022/07/31.
+//
+
+import Foundation
+
+class BookURL {
+
+    let value: String
+
+    init (value: String) {
+        self.value = value
+    }
+
+    func toString() -> String {
+        return self.value
+    }
+
+}
